e133717a3c5268aec8919d7a964285e5e8a803e6
Aimlessfish <millweekin@gmail.com>
2024-11-21 16:18:05 +0000

    Created directory creation example

---
+++ directories.go
@@ -0,0 +1,23 @@
+// Here is an example of creating directories with permissions
+// requires elevated permissions to run
+
+package main
+
+import (
+	"log/slog"
+	"os"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+	logger = slog.With("LogID", "directories")
+
+	path := "/home/example"
+	err := os.MkdirAll(path, 0755)
+	if err != nil {
+		logger.Warn("Error running os.MkdirAll", "error", err.Error())
+		os.Exit(1)
+	}
+
+}
