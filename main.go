39a09e4d5ff46b0761cf61d540476c0f9487cc6c
Aimlessfish <millweekin@gmail.com>
2024-11-21 12:14:46 +0000

    basic flag exmaples

---
+++ main.go
@@ -0,0 +1,21 @@
+// Example of using argument flags in Go.
+// Get, parse, output parse.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// Get
+	flag1 := flag.String("flag1", "", "string")
+	flag2 := flag.Int("flag2", 0, "integer")
+
+	// Parse
+	flag.Parse()
+
+	// Output
+	fmt.Print("flag1 = ", *flag1, " flag2 = ", *flag2)
+}
