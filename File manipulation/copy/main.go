facc592a86dd90a75e2c6be2b95dc018dfab23cb
Aimlessfish <millweekin@gmail.com>
2024-11-29 14:55:14 +0000

    removed depreciated ioutil

--- File manipulation/copy/main.go
+++ File manipulation/copy/main.go
@@ -1,31 +1,31 @@
 package main
 
-import(
+import (
+	"flag"
 	"log/slog"
-	"io"
-	"fmt"
+	"os"
 )
 
-func copyFile(src string, dest string) error{
+func copyFile(src string, dest string) error {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 	logger = slog.With("logID", "copyFile")
-	
-	bytesRead, err := ioutil.ReadFile(src)
+
+	bytesRead, err := os.ReadFile(src)
 	if err != nil {
 		logger.Warn("Error reading source file", err.Error())
 		return err
 	}
 
-	err = ioutil.WriteFile(dest, bytesRead, 0644)
+	err = os.WriteFile(dest, bytesRead, 0644)
 	if err != nil {
 		logger.Warn("Error writing destination file", err.Error())
 		return err
 	}
-	
+
 }
 
-func main(){
+func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 	logger = slog.With("logID", "Main")
@@ -33,9 +33,10 @@ func main(){
 	source := flag.String("Source", "", "Source File Location")
 	destination := flag.String("Destination", "", "Destination")
 	flag.Parse()
-	
-	err := copyFile(source, destination)
+
+	err := copyFile(*source, *destination)
 	if err != nil {
 		logger.Warn("Error running copyFile", err.Error())
 	}
+
 }
