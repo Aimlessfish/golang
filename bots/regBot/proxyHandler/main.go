3b1dd6e7c940bcb5e9dd0aa05f9281b47777082a
Aimlessfish <millweekin@proton.me>
2025-08-11 14:09:16 +0100

    change linxu driver pack

--- bots/regBot/proxyHandler/main.go
+++ bots/regBot/proxyHandler/main.go
@@ -20,7 +20,7 @@ import (
 
 const (
 	WIN_GECKO_PATH   = "C:\\Users\\notWill\\Documents\\GitHub\\automation\\golang\\bots\\regBot\\bin\\geckodriver.exe"
-	LINUX_GECKO_PATH = "./bin/geckodriver"
+	LINUX_GECKO_PATH = "../bin/geckodriver"
 	GECKO_PORT       = 4444
 	PROXY_SCRAPE_API = "https://api.proxyscrape.com/v4/free-proxy-list/get?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all&skip=0&limit=500"
 	PUB_PROXY_API    = "http://pubproxy.com/api/proxy?type=http"
@@ -151,7 +151,6 @@ func APICall(osType string) ([]string, error) { // get proxies
 	}
 
 	proxies := strings.Split(strings.TrimSpace(string(bodyText)), "\n")
-
 	if osType == "darwin" {
 		os.Exit(1)
 	}
