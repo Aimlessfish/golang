ae5e74dadca0db9f9586bdc71c7fc2ed0e69b647
Aimlessfish <millweekin@proton.me>
2025-08-13 14:04:51 +0100

    Finished.

--- utilities/proxyHandler/main.go
+++ utilities/proxyHandler/main.go
@@ -1,24 +1,6 @@
 /* Written by Will Meekin */
 /* This scrapes two proxy api providers and returns as many working proxies as possile */
 
-// production only
-
-/*
-type flags struct {
-	returnType int
-}
-*/
-
-/*
-	 change this to getProxy() ([]string, error) {
-		flag.int("type", 0, "return type 0 == 1 PROXY || 1 == LIST OF PROXIES")
-		if flag.Int != 1 {
-
-		*** main logic here ***
-
-		return proxies, nil
-	}
-*/
 package main
 
 import (
