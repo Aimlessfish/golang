da27a2ae9f8c52b9d571c1466982c5486df87eb2
Aimlessfish <millweekin@proton.me>
2025-08-16 22:39:22 +0100

    new functions persistant open bot connection

--- bots/discordBot/bot/main.go
+++ bots/discordBot/bot/main.go
@@ -2,17 +2,69 @@ package bot
 
 import (
 	initlogger "golang/utilities/initLogger"
+	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
-func GetBotToken() (string, error) {
+func getToken() string {
 	logger := initlogger.LoggerInit("GET BOT", "BOT")
 	logger.Info("Getting bot token")
-	token := os.Getenv("BOT_TOKEN")
-	if token != "" {
-		logger.Info("INVALID BOT TOKEN", "ERROR", token)
-		return "INVALID BOT TOKEN", nil
 
+	err := godotenv.Load()
+	if err != nil {
+		logger.Info("INVALID BOT TOKEN", "ERROR", err.Error())
+		return "broke mate"
+	} else {
+		token := os.Getenv("TOKEN")
+		if len(token) == 0 {
+			panic("Token length == 0!")
+		} else {
+			return token
+		}
 	}
-	return token, nil
+}
+
+func ConnectAPI(logger *slog.Logger) error {
+	logger = logger.With("Bot", "ConnectAPI")
+	api_key := getToken()
+
+	discord, err := discordgo.New("Bot " + api_key)
+	if err != nil {
+		logger.Error("API connect failed!")
+		return err
+	}
+	err = discord.Open()
+	if err != nil {
+		panic(err)
+	}
+	discord.AddHandler(messageHandler)
+	logger.Info("Bot is running. Press CTRL+C to exit.")
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-stop
+
+	return nil
+}
+
+func messageHandler(server *discordgo.Session, message *discordgo.MessageCreate) {
+
+	if message.Author.ID == server.State.User.ID {
+		return
+	}
+
+	server.ChannelMessageSendReply(message.ChannelID, "Not ready yet", &discordgo.MessageReference{
+		MessageID: message.ID,
+		ChannelID: message.ChannelID,
+		GuildID:   message.GuildID,
+	})
+
+	// if message.Content == "start" {
+	// 	server.ChannelMessageSend(message.ChannelID, "Not ready yet!")
+	// }
+
 }
--- bots/discordBot/main.go
+++ bots/discordBot/main.go
@@ -1,12 +1,16 @@
-package discordbot
+package main
 
 import (
+	bot "golang/bots/discordBot/bot"
 	initlogger "golang/utilities/initLogger"
 )
 
 func main() {
 	logger := initlogger.LoggerInit("MAIN", "MAIN")
 
-	logger.Debug("DiscordBot! Started")
+	err := bot.ConnectAPI(logger)
+	if err != nil {
+		panic(err)
+	}
 
 }
